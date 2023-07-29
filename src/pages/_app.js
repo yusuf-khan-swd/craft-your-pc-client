e8ea52f3dc19fa5b5f90892ccc5cc06973e2721a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-29 20:33:54 +0600

    add next provider and api auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <SessionProvider session={pageProps.session}>
+      {getLayout(<Component {...pageProps} />)}
+    </SessionProvider>
+  );
 }
