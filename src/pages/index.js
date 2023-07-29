41a56fb056ae645d51610a28470bfa86b6fed682
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-29 15:34:17 +0600

    add daisyui to project and its working

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,13 @@ const categories = [
 const HomePage = () => {
   return (
     <div>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
       <h1>Home Page</h1>
       <h2>Featured Products</h2>
       <div>
