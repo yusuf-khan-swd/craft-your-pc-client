c8618e96f01005ffdaafe75a47a315503ff61c35
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-30 00:06:14 +0600

    show categories

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import RootLayout from "@/components/Layouts/RootLayout";
 import ProductCard from "@/components/UI/ProductCard";
 import Head from "next/head";
+import Link from "next/link";
 
 const HomePage = ({ products, categories }) => {
   console.log("Inside HomePage products", products);
@@ -17,14 +18,33 @@ const HomePage = ({ products, categories }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="grid grid-cols-3 gap-5 p-10 w-[80%] mx-auto">
-        {products && products.length > 0 ? (
-          products.map((product) => (
-            <ProductCard key={product._id} product={product} />
-          ))
-        ) : (
-          <p>No products available.</p>
-        )}
+      <div className="w-[80%] mx-auto">
+        <h1>Featured Products</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6  mb-16 mt-4">
+          {products && products.length > 0 ? (
+            products.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))
+          ) : (
+            <p>No products available.</p>
+          )}
+        </div>
+        <h1></h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 md:gap-2 m-2 mb-16 mx-auto">
+          {categories && categories.length > 0 ? (
+            categories.map((category) => (
+              <Link
+                href={`/category/${category._id}`}
+                className="btn m-1"
+                key={category._id}
+              >
+                {category.category_name}
+              </Link>
+            ))
+          ) : (
+            <p>No categories available.</p>
+          )}
+        </div>
       </div>
     </>
   );
