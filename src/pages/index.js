11650e3e51048f9972c073814b363d071512e802
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-29 20:04:44 +0600

    get static props not working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import RootLayout from "@/components/layout/RootLayout";
+// import RootLayout from "@/components/Layouts/RootLayout";
 import { useSession } from "next-auth/react";
+import Head from "next/head";
 import Link from "next/link";
-import React from "react";
 
 const categories = [
   "CPU / Processor",
@@ -13,11 +13,20 @@ const categories = [
   "Others",
 ];
 
-const HomePage = () => {
+const HomePage = ({ abc }) => {
+  console.log("Inside HomePage", abc);
   const { data: session } = useSession();
-
   return (
-    <div>
+    <>
+      <Head>
+        <title>Home - CraftYourPC</title>
+        <meta
+          name="description"
+          content="This is a PC Builder Website made by next-js"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>Home Page {session?.user.email}</h1>
       <h2>Featured Products</h2>
       <div>
@@ -30,12 +39,21 @@ const HomePage = () => {
           <button className="btn m-1">{category}</button>
         </Link>
       ))}
-    </div>
+    </>
   );
 };
-
 export default HomePage;
 
-HomePage.getLayout = function getLayout(page) {
-  return <RootLayout>{page}</RootLayout>;
+// HomePage.getLayout = function getLayout(page) {
+//   return <RootLayout>{page}</RootLayout>;
+// };
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/products");
+  const products = await res.json();
+  console.log("Inside getStaticProps", products?.data?.length);
+  return {
+    props: { abc: "abc" },
+    revalidate: 30,
+  };
 };
