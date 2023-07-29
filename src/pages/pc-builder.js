34f0dc7e40216f1ad927002bf0d755f6042d403d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-30 00:06:36 +0600

    add root layout to PCBuilderPage

--- src/pages/pc-builder.js
+++ src/pages/pc-builder.js
@@ -1,11 +1,16 @@
+import RootLayout from "@/components/Layouts/RootLayout";
 import React from "react";
 
 const PCBuilderPage = () => {
   return (
-    <div>
+    <div className="container">
       <h1>PC Builder Page</h1>
     </div>
   );
 };
 
 export default PCBuilderPage;
+
+PCBuilderPage.getLayout = function (page) {
+  return <RootLayout>{page}</RootLayout>;
+};
