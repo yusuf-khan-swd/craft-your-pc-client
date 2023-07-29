16e2eb99cbdca5ebf8df2251dab9bb26cbeefad6
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-30 02:14:02 +0600

    show product of the specific category

--- src/pages/category/[id].js
+++ src/pages/category/[id].js
@@ -1,16 +1,22 @@
 import RootLayout from "@/components/Layouts/RootLayout";
-import { useRouter } from "next/router";
+import ProductCard from "@/components/UI/ProductCard";
 import React from "react";
 
 const CategoryDetailsPage = ({ products, category }) => {
-  const router = useRouter();
-
   console.log("inside category details page", products);
   console.log("inside category details page", category);
 
   return (
-    <div>
-      <h1>Category Details Page {router.query.id}</h1>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6  mb-16 mt-4">
+        {products && products.length > 0 ? (
+          products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))
+        ) : (
+          <p>No products available.</p>
+        )}
+      </div>
     </div>
   );
 };
@@ -38,11 +44,13 @@ export const getStaticProps = async (context) => {
     `http://localhost:5000/category-products/${params.id}`
   );
   const data = await res.json();
+  const products = data?.data?.products;
+  const category = data?.data?.category;
 
   return {
     props: {
-      products: data.data,
-      category: data.category,
+      products,
+      category,
     },
   };
 };
