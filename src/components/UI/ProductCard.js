a8ef1738183d66f25f1f8ca74d746cb73c468577
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-30 14:31:29 +0600

    modified product card style

--- src/components/UI/ProductCard.js
+++ src/components/UI/ProductCard.js
@@ -3,15 +3,25 @@ const ProductCard = ({ product }) => {
   return (
     <div className="rounded-2xl h-[480px] flex flex-col items-start justify-between p-5 overflow-hidden shadow-md border border-gray-100 hover:shadow-2xl hover:scale-[102%] transition-all">
       <Link href={`/product/${product._id}`} className="w-full">
-        <img src={product?.image} alt="product" width={300} />
-        <h1 className="text-xl font-semibold">{product?.name}</h1>
+        <div className="w-[90%] mx-auto mb-4">
+          <img
+            src={product?.image}
+            alt="product"
+            width={300}
+            height={180}
+            className="h-[280px] mx-auto"
+          />
+        </div>
+        <h1 className="text-xl font-semibold mb-2">{product?.name}</h1>
       </Link>
-      <p>Rating: {product?.rating}</p>
-      <p className="text-sm">
-        Availability: {product?.status ? "In stock" : "Out of stock"}
-      </p>
-      <p className="text-sm">Price: {product?.price}</p>
-      <button className="btn btn-neutral">Add to cart</button>
+      <div className="space-y-1">
+        <p>Category: {product?.category}</p>
+        <p>Rating: {product?.rating}</p>
+        <p className="text-sm">Price: ${product?.price}</p>
+        <p className="text-sm">
+          Status : {product?.status ? "In stock" : "Out of stock"}
+        </p>
+      </div>
     </div>
   );
 };
