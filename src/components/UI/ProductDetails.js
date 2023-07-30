56b9fc6380083000e4886916e2bc0d4d5fe31ee4
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-30 19:41:04 +0600

    Modify some style in Product Details page and show more property and remove some property and set order by task description

--- src/components/UI/ProductDetails.js
+++ src/components/UI/ProductDetails.js
@@ -5,21 +5,32 @@ const ProductDetails = ({ product }) => {
         <div className="w-full mx-auto m-4">
           <img src={product?.image} alt="" />
         </div>
-        <div className="m-4 md:m-8 space-y-3 mx-auto">
+        <div className="m-4 md:m-8 space-y-2 mx-auto">
           <h1 className="text-3xl font-semibold">{product?.name}</h1>
-          <p className="text-xl">Rating: {product?.rating}</p>
-          <ul className="space-y-2 text-lg">
+          <p className="text-lg">Category: {product?.category}</p>
+          <p className="text-lg">
+            Status: {product?.status ? "In stock" : "Out of stock"}
+          </p>
+          <p className="text-lg">Price: ${product?.price}</p>
+          <p className="text-lg">
+            Description: <span>{product?.description}</span>
+          </p>
+          <ul className="space-y-2 text-lg py-2">
+            <p className="font-bold">Key Featured</p>
             {product?.features?.map((feature) => (
-              <li key={feature}>
-                <b>{feature.split(":")[0]}: </b>
+              <li key={feature} className="ml-4">
+                <span>{feature.split(":")[0]}: </span>
                 {feature.split(":")[1]}
               </li>
             ))}
           </ul>
-          <p>Description: {product?.description}</p>
-          <p>Individual Rating: {product?.individualRating}</p>
-          <p>Average Rating: {product?.averageRating}</p>
-          <p>Reviews: {product?.reviews}</p>
+          <p className="text-lg">
+            Individual Rating: {product?.individualRating} (Out of 5 Stars)
+          </p>
+          <p className="text-lg">
+            Average Rating: {product?.averageRating} (Out of 5 Stars)
+          </p>
+          <p className="text-lg">Reviews: {product?.reviews} </p>
         </div>
       </div>
     </div>
